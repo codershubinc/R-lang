02dd80ad82c15173a6257f450c0444f8f4e4c678
swapnil ingle <ingleswapnil2004@gmail.com>
2025-02-01 22:13:10 +0530

    Add variable examples and operations in R scripts

--- learning/basics/variables.r
+++ learning/basics/variables.r
@@ -1,3 +1,69 @@
+# int variable
+print("int variable")
 t <- 6
 
+t
+
+# double variable
+print("double variable")
+pi <- 3.14
+
+pi
+
+# character variable
+print("character variable")
+name <- "John"
+name
+
+# logical variable
+print("logical variable")
+is_true <- TRUE
+is_true
+
+# complex variable
+print("complex variable")
+complex <- 1 + 2i
+complex
+
+# vector
+print("vector")
+v <- c(1, 2, 3, 4, 5)
+v
+
+# matrix
+print("matrix")
+m <- matrix(1:9, nrow = 3, ncol = 3)
+m
+
+# data frame
+print("data frame")
+df <- data.frame(
+  name = c("John", "Jane", "Joe"),
+  age = c(25, 30, 35),
+  dnc = c(TRUE, FALSE, TRUE)
+)
+df
+
+# list
+print("list")
+l <- list(
+  name = "John",
+  age = 25,
+  dnc = TRUE
+)
+l
+
+# array
+print("array")
+a <- array(1:24, dim = c(2, 3, 4))
+a
+
+# factor
+print("factor")
+f <- factor(c("A", "B", "C", "A", "B", "C"))
+f
+
+# table
+print("table")
+t <- table(c("A", "B", "C", "A", "B", "C"))
 t
