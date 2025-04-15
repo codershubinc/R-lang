7ff58645ad9fe5d33a83d97e1ca95c3b3369df68
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-04-15 19:33:28 +0530

    Refactor code structure for improved readability and maintainability

--- r/01.r
+++ r/01.r
@@ -1,2 +1,40 @@
-r <- 60
-r
+
+dt <- data.frame(
+    name=c("John", "Jane", "Jim"),
+    age=c(30, 25, 35)
+    # height=c(180, 170, 175),
+)
+
+print('data')
+print(dt)
+
+
+library('ggplot2')
+
+average_age <- mean(dt$age)
+
+cat('Average age:', average_age)
+
+ggplot(
+    data=dt,
+    aes(x=name, y=age),
+    label=name,
+)+
+geom_point(
+    color='blue',
+    size=3
+)+
+geom_text(
+    nudge_y=4,
+    color='black',
+    label=dt$name,
+)+
+labs(
+    title='Ages of people',
+    x='Name',
+    y='Age'
+)
+theme_minimal()
+ggsave(
+    filename='plot.png', 
+)
